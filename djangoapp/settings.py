7312384828afd13ef0bd853b91e8cfd7e3398dbb
engine07 <psihodelik@gmail.com>
2014-07-22 10:15:37 +0200

    updated settings

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -80,3 +80,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
